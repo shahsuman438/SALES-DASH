1d44b7c5878acc0001431993a416147f705ff3fe
shahsuman438 <shahsuman438@gmail.com>
2024-03-17 18:58:31 +0545

    fetch all transactions

--- server/pkg/sales/sales.controller.go
+++ server/pkg/sales/sales.controller.go
@@ -1,7 +1,20 @@
 package sales
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/shahsuman438/SALES-DASH/CORE-API/pkg/response"
+)
 
 func setupControllers(engine *gin.Engine) {
-	engine.Group("/sales")
+	r := engine.Group("/sales")
+	r.GET("", GetAllSales)
+}
+
+func GetAllSales(c *gin.Context) {
+	data, err := Fetch(c)
+	if err != nil {
+		response.BadRequest(c, err)
+		return
+	}
+	response.Success(c, data)
 }
--- server/pkg/sales/sales.services.go
+++ server/pkg/sales/sales.services.go
@@ -2,11 +2,15 @@ package sales
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/mitchellh/mapstructure"
 	"github.com/shahsuman438/SALES-DASH/CORE-API/pkg/database"
+	"github.com/shahsuman438/SALES-DASH/CORE-API/pkg/utils/logger"
 )
 
+var collectionName = "Sales"
+
 func Add(ctx *gin.Context, data *Sales) error {
-	err := database.Save(ctx, data, "Sales")
+	err := database.Save(ctx, data, collectionName)
 	return err
 }
 
@@ -15,8 +19,24 @@ func AddMany(ctx *gin.Context, data *[]Sales) error {
 	for i, v := range *data {
 		interfaceData[i] = v
 	}
-	err := database.SaveMany(ctx, interfaceData, "Sales")
+	err := database.SaveMany(ctx, interfaceData, collectionName)
 	return err
 }
 
-
+func Fetch(ctx *gin.Context) ([]Sales, error) {
+	data, err := database.Fetch(ctx, collectionName)
+	if err != nil {
+		logger.Error("Error fetching from Product", err)
+		return nil, err
+	}
+	var transactions []Sales
+	for _, item := range data {
+		var sale Sales
+		if err := mapstructure.Decode(item, &sale); err != nil {
+			logger.Error("Error decoding product", err)
+			return nil, err
+		}
+		transactions = append(transactions, sale)
+	}
+	return transactions, nil
+}
